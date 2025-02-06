04b0a42d7f0dae2730a5f14d68ed9b7e77ebd07f
Shannon Leonard <121053869+shannonleonard@users.noreply.github.com>
2025-02-06 10:00:07 -0800

    fix

--- src/VerticalSlideshow.js
+++ src/VerticalSlideshow.js
@@ -1,16 +1,1 @@
-import React from 'react';
-
-function VerticalSlideshow({ currentSlide, setCurrentSlide }) {
-  return (
-    <div className="vertical-slideshow">
-      {/* Other slide content... */}
-      <div className="creative-workflow-lab-url mt-[50px]">
-        <a href="https://creativeworkflowlab.com" target="_blank" rel="noreferrer">
-          Creative Workflow Lab
-        </a>
-      </div>
-    </div>
-  );
-}
-
-export default VerticalSlideshow; 
+ 
